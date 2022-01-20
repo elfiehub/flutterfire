a3419c9321ce7b3c4b245a11387f867db7e26e9d
Khang Nguyen <khang.nguyen.dev@gmail.com>
2022-01-20 15:26:52 +0700

    Revert "fix ios notification in foreground not working"
    This reverts commit 49b9dc828d7da3930776245f1ae80d6435c7f846.

--- packages/firebase_messaging/firebase_messaging/ios/Classes/FLTFirebaseMessagingPlugin.m
+++ packages/firebase_messaging/firebase_messaging/ios/Classes/FLTFirebaseMessagingPlugin.m
@@ -300,11 +300,6 @@ NSString *const kMessagingPresentationOptionsUserDefaults =
     [_originalNotificationCenterDelegate userNotificationCenter:center
                                         willPresentNotification:notification
                                           withCompletionHandler:completionHandler];
-    UNNotificationPresentationOptions presentationOptions = UNNotificationPresentationOptionNone;
-    presentationOptions |= UNNotificationPresentationOptionAlert;
-    presentationOptions |= UNNotificationPresentationOptionBadge;
-    presentationOptions |= UNNotificationPresentationOptionSound;
-    completionHandler(presentationOptions);
   } else {
     UNNotificationPresentationOptions presentationOptions = UNNotificationPresentationOptionNone;
     NSDictionary *persistedOptions = [[NSUserDefaults standardUserDefaults]
